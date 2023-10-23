c2e8638f3db2654fabe13ae8641061c6f826ff47
Andriy Meleniuk <amelentium@gmail.com>
2023-10-23 23:38:05 +0300

    Add songs parsing functionality

--- TestAssignment/Constants.cs
+++ TestAssignment/Constants.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestAssignment
+﻿namespace TestAssignment
 {
     public static class Constants
     {
-        public static string[] PlaylistUrls = { 
+        public static string[] PlaylistUrls = {
             "https://music.amazon.com/playlists/B01M11SBC8",
             "https://music.amazon.com/albums/B001230JXC",
         };
 
         public static class Amazon
         {
-            public const string PlaylistInfoSelector = "music-detail-header";
+            public const string PlaylistInfoXPath = "//music-detail-header";
             public const string PlatlistTypeAttribute = "label";
             public const string PlatlistIconAttribute = "image-src";
             public const string PlatlistNameAttribute = "headline";
             public const string PlatlistDescriptionAttribute = "secondary-text";
 
-            public const string PlaylistSongsContainerSelector = "music-container";
+            public const string SongInfoSelector = "music-text-row";
+            public const string SongInfoXPath = $"//{SongInfoSelector}";
+            public const string SongNameAttribute = "primary-text";
+            public const string SongArtistNameAttribute = "secondary-text-1";
+            public const string SongAlbumNameAttribute = "secondary-text-2";
+            public const string SongDurationXPath = "//div/div/music-link";
+            public const string SongDurationAttribute = "title";
 
         }
     }
--- TestAssignment/Parser/AmazonPlaylistParser.cs
+++ TestAssignment/Parser/AmazonPlaylistParser.cs
@@ -1,6 +1,6 @@
 ﻿using HtmlAgilityPack;
 using PuppeteerSharp;
-using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TestAssignment.Models;
@@ -9,9 +9,10 @@ namespace TestAssignment.Parser
 {
     public static class AmazonPlaylistParser
     {
-
         public async static Task<PlaylistModel?> ParsePlaylistAsync(string playlistUrl)
         {
+            PlaylistModel? playlist = null;
+
             try
             {
                 using var browserFetcher = new BrowserFetcher();
@@ -28,30 +29,78 @@ namespace TestAssignment.Parser
 
                 await page.WaitForNetworkIdleAsync();
                 var html = await page.GetContentAsync();
-                browser.CloseAsync();
 
                 var doc = new HtmlDocument();
                 doc.LoadHtml(html);
-                var playlistInfoNodes = doc.DocumentNode.SelectNodes($"//{Constants.Amazon.PlaylistInfoSelector}");
+                var playlistInfoNodes = doc.DocumentNode.SelectNodes(Constants.Amazon.PlaylistInfoXPath);
                 var playlistInfo = playlistInfoNodes.FirstOrDefault(x => x.Attributes.Contains(Constants.Amazon.PlatlistNameAttribute));
 
-                if (playlistInfo != null)
+                if (playlistInfo == null)
                 {
-                    return new PlaylistModel
-                    {
-                        Type = playlistInfo.GetAttributeValue(Constants.Amazon.PlatlistTypeAttribute, string.Empty),
-                        Icon = playlistInfo.GetAttributeValue(Constants.Amazon.PlatlistIconAttribute, string.Empty),
-                        Name = playlistInfo.GetAttributeValue(Constants.Amazon.PlatlistNameAttribute, string.Empty),
-                        Description = playlistInfo.GetAttributeValue(Constants.Amazon.PlatlistDescriptionAttribute, string.Empty)
-                    };
+                    return null;
                 }
 
-                return null;
+                playlist = new PlaylistModel
+                {
+                    Type = playlistInfo.GetAttributeValue(Constants.Amazon.PlatlistTypeAttribute, string.Empty),
+                    Icon = playlistInfo.GetAttributeValue(Constants.Amazon.PlatlistIconAttribute, string.Empty),
+                    Name = playlistInfo.GetAttributeValue(Constants.Amazon.PlatlistNameAttribute, string.Empty),
+                    Description = playlistInfo.GetAttributeValue(Constants.Amazon.PlatlistDescriptionAttribute, string.Empty),
+                };
+
+                playlist.Songs = await ParsePlaylistSongs(page);
+
+                await browser.CloseAsync();
+
+                return playlist;
             }
             catch
             {
-                return null;
+                return playlist;
             }
+        }
+
+        private async static Task<List<SongModel>> ParsePlaylistSongs(IPage page)
+        {
+            var repeatCount = 0;
+            var doc = new HtmlDocument();
+            var songs = new List<SongModel>();
+
+            while (repeatCount < 3)
+            {
+                repeatCount++;
+                var html = await page.GetContentAsync();
+                doc.LoadHtml(html);
+                var songInfoNodes = doc.DocumentNode.SelectNodes(Constants.Amazon.SongInfoXPath);
+                var songInfos = songInfoNodes.Where(x => x.Attributes.Contains(Constants.Amazon.SongNameAttribute));
+
+                foreach (var songInfo in songInfos)
+                {
+                    var outerDoc = new HtmlDocument();
+                    outerDoc.LoadHtml(songInfo.OuterHtml);
+                    var songName = songInfo.GetAttributeValue(Constants.Amazon.SongNameAttribute, string.Empty);
+
+                    if (!string.IsNullOrWhiteSpace(songName) && !songs.Any(x => x.Name == songName))
+                    {
+                        var duration = outerDoc.DocumentNode
+                            .SelectNodes(Constants.Amazon.SongDurationXPath)
+                            .LastOrDefault()
+                            ?.GetAttributeValue(Constants.Amazon.SongDurationAttribute, null) ?? string.Empty;
+
+                        songs.Add(new SongModel
+                        {
+                            Name = songName,
+                            Duration = duration,
+                            AlbumName = songInfo.GetAttributeValue(Constants.Amazon.SongAlbumNameAttribute, string.Empty),
+                            ArtistName = songInfo.GetAttributeValue(Constants.Amazon.SongArtistNameAttribute, string.Empty),
+                        });
+
+                        repeatCount = 0;
+                    }
+                }
+            }
+
+            return songs;
         }
     }
 }
